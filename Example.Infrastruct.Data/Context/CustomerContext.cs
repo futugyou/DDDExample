6a96954a51a30b400b072e6735a5040455d25756
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2020-12-04 18:37:53 +0000

    add efcore interceptor
    add efcore interceptor

--- Example.Infrastruct.Data/Context/CustomerContext.cs
+++ Example.Infrastruct.Data/Context/CustomerContext.cs
@@ -9,14 +9,29 @@ namespace Example.Infrastruct.Data.Context
 {
     public class CustomerContext : DbContext
     {
-        public DbSet<Customer> Customers { get; set; }   
+        public CustomerContext()
+        {
+            ChangeTracker.StateChanged += ChangeTracker_StateChanged;
+            ChangeTracker.Tracked += ChangeTracker_Tracked;
+        }
+
+        private void ChangeTracker_Tracked(object sender, Microsoft.EntityFrameworkCore.ChangeTracking.EntityTrackedEventArgs e)
+        {
+
+        }
+
+        private void ChangeTracker_StateChanged(object sender, Microsoft.EntityFrameworkCore.ChangeTracking.EntityStateChangedEventArgs e)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new CustomerMap());
-                        
-            base.OnModelCreating(modelBuilder);
-        } 
 
+            base.OnModelCreating(modelBuilder);
+        }
         /// <summary>
         /// 重写连接数据库
         /// </summary>
@@ -32,6 +47,11 @@ namespace Example.Infrastruct.Data.Context
             // 定义要使用的数据库
             //optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
             optionsBuilder.UseSqlite("Data Source = ddd_demo.db");
-        } 
+            optionsBuilder.AddInterceptors(_commandInterceptor, _connectInterceptor, _transactionInterceptor);
+        }
+
+        private static readonly TaggedQueryCommandInterceptor _commandInterceptor = new TaggedQueryCommandInterceptor();
+        private static readonly TaggedConnectionInterceptor _connectInterceptor = new TaggedConnectionInterceptor();
+        private static readonly TaggedTransactionInterceptor _transactionInterceptor = new TaggedTransactionInterceptor();
     }
 }
