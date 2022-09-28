a10b12996a43d359b826dbcc0d71b2b56f2a58eb
zhaokangcheng <17871902+futugyou@users.noreply.github.com>
2022-09-28 10:39:21 +0800

    TEC-4 completion the customer application test
    TEC-4 completion the customer application test

---
+++ src/Example.Application.UnitTest/UpdateCustomerUnitTest.cs
@@ -0,0 +1,42 @@
+﻿namespace Example.Application.UnitTest;
+public class UpdateCustomerUnitTest
+{
+    [Fact]
+    public async Task UpdateCustomerSuccessTest()
+    {
+        // Arrage
+        var _mediatorHandler = new Mock<IMediatorHandler>();
+        var _customerRepository = new Mock<ICustomerRepository>();
+        var customerViewModel = new CustomerViewModel
+        {
+            BirthDate = DateTime.Now,
+            City = "shanghai",
+            County = "xuhuiqu",
+            Email = "shanghai@sh.com",
+            Name = "wangxiaoming",
+            Province = "shanghai",
+            Street = "tianlinlu 100 hao",
+        };
+
+        // Act
+        ICustomerAppService service = new CustomerAppService(_customerRepository.Object, AutoMapperHelper.mapper, _mediatorHandler.Object);
+        await service.Update(customerViewModel);
+
+        // Assert
+        Assert.True(true);
+    }
+
+    [Fact]
+    public async Task UpdateCustomerFailedTest()
+    {
+        // Arrage
+        var _mediatorHandler = new Mock<IMediatorHandler>();
+        var _customerRepository = new Mock<ICustomerRepository>();
+
+        // Act
+        ICustomerAppService service = new CustomerAppService(_customerRepository.Object, AutoMapperHelper.mapper, _mediatorHandler.Object);
+
+        // Assert
+        await Assert.ThrowsAsync<ArgumentNullException>(() => service.Update(null));
+    }
+}
