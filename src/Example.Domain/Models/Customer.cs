fcae30bac282526683199006849b05eb451f8c1a
zhaokangcheng <17871902+futugyou@users.noreply.github.com>
2022-09-28 16:26:12 +0800

    TEC-5 add customer domain unittest
    TEC-5 add customer domain unittest

--- src/Example.Domain/Models/Customer.cs
+++ src/Example.Domain/Models/Customer.cs
@@ -5,16 +5,35 @@
 /// </summary>
 public class Customer : Entity
 {
+    private const int MinNameLenght = 5;
+    private const int MaxNameLenght = 100;
+
     protected Customer() { }
     public Customer(Guid id, string name, string email, DateTime birthDate)
     {
+        if (id == Guid.Empty)
+        {
+            throw new ArgumentNullException(nameof(id));
+        }
+
         Id = id;
+
+        if (name?.Length < MinNameLenght)
+        {
+            throw new AggregateException("name is too short");
+        }
+
+        if (name?.Length > MaxNameLenght)
+        {
+            throw new AggregateException("name is too long");
+        }
+
         Name = name;
-        Email = email;
+        Email = email ?? throw new ArgumentNullException(nameof(email));
         BirthDate = birthDate;
         Address = new Address();
-    } 
-    public Address Address { get; set; }     
+    }
+    public Address Address { get; set; }
     public string Name { get; private set; }
     public string Email { get; private set; }
     public DateTime BirthDate { get; private set; }
