d4255c2e5d70c5e6580c1ce43dc3594691d6bd7b
zhaokangcheng <17871902+futugyou@users.noreply.github.com>
2022-10-21 18:13:55 +0800

    add enumeration unit test
    add enumeration unit test

---
+++ src/Example.Domain.UnitTest/EnumTest.cs
@@ -0,0 +1,111 @@
+﻿namespace Example.Domain.UnitTest;
+
+public class EnumTest
+{
+    [Fact]
+    public void EnumTostringShouldEqualName()
+    {
+        // Arrage
+        var id = 1;
+        var name = "name";
+        var e = new StubEnum(id, name);
+        var tostring = e.ToString();
+        // Act
+
+        // Assert
+        Assert.NotNull(tostring);
+        Assert.True(name == tostring);
+    }
+
+    [Fact]
+    public void GetAllShouldReturnAllEnumType()
+    {
+        // Arrage
+        var enums = Enumeration.GetAll<StubEnum>();
+        // Act
+
+        // Assert
+        Assert.NotNull(enums);
+        Assert.True(1 == enums.Count());
+    }
+
+    [Fact]
+    public void FromValuWithExistIDeShouldReturnEnum()
+    {
+        // Arrage
+        var s = Enumeration.FromValue<StubEnum>(1);
+        // Act
+
+        // Assert
+        Assert.NotNull(s);
+        Assert.True(1 == s.Id);
+    }
+
+    [Fact]
+    public void FromValuWithNotexistIDeShouldThrowInvalidOperationException()
+    {
+        // Arrage
+        // Act
+
+        // Assert
+        Assert.Throws<InvalidOperationException>(() => Enumeration.FromValue<StubEnum>(2));
+    }
+
+    [Fact]
+    public void FromValuWithExistNameeShouldReturnEnum()
+    {
+        // Arrage
+        var s = Enumeration.FromDisplayName<StubEnum>("submitted");
+        // Act
+
+        // Assert
+        Assert.NotNull(s);
+        Assert.True("submitted" == s.Name);
+    }
+
+    [Fact]
+    public void FromValuWithNotexistNameeShouldThrowInvalidOperationException()
+    {
+        // Arrage
+        // Act
+
+        // Assert
+        Assert.Throws<InvalidOperationException>(() => Enumeration.FromDisplayName<StubEnum>("Submitted1"));
+    }
+
+    [Fact]
+    public void EqualIDAndEqualTypeShouldEqualEnum()
+    {
+        // Arrage
+        var id = 1;
+        var name = "name";
+        var e = new StubEnum(id, name);
+        var f = new StubEnum(id, name);
+        // Act
+
+        // Assert
+        Assert.True(e == f);
+        Assert.True(e.Equals(f));
+        Assert.False(e.Equals(new object()));
+        Assert.False(null == f);
+        Assert.False(e == null);
+        Assert.True(e.GetHashCode() == f.GetHashCode());
+    }
+
+    [Fact]
+    public void NotEqualIDOrNotEqualTypeShouldNotEqualEnum()
+    {
+        // Arrage
+        var e = new StubEnum(1, "name");
+        var f = new StubEnum(2, "name");
+        var e1 = new StubEnum1(1, "name");
+        // Act
+
+        // Assert
+        Assert.True(e != f);
+        Assert.True(!e.Equals(f));
+
+        Assert.True(e != e1);
+        Assert.True(!e.Equals(e1));
+    }
+}
---
+++ src/Example.Domain.UnitTest/Stub/StubEnum.cs
@@ -0,0 +1,16 @@
+﻿namespace Example.Domain.UnitTest;
+
+public class StubEnum : Enumeration
+{
+    public static StubEnum Submitted = new StubEnum(1, nameof(Submitted).ToLowerInvariant());
+    public StubEnum(int id, string name) : base(id, name)
+    {
+    }
+}
+
+public class StubEnum1 : Enumeration
+{
+    public StubEnum1(int id, string name) : base(id, name)
+    {
+    }
+}
